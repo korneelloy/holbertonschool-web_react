5a12c61800b2b0fded42a36f18e707892e2aef83
korneel <korneel.loy@gmail.com>
2025-05-26 14:58:13 +0200

    typescript ex 7

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -56,5 +56,12 @@ function executeWork(employee: Director | Teacher) {
   }
 }
 
-executeWork(createEmployee(200));   // Outputs: Getting to work
-executeWork(createEmployee(1000));  // Outputs: Getting to director tasks
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return('Teaching Math');
+  } else {
+    return('Teaching History');
+  }
+}
