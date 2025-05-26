711c46b1a5be9ad4e3f98c0fbcf744990b318cba
korneel <korneel.loy@gmail.com>
2025-05-26 14:49:15 +0200

    typescript ex 6

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -36,10 +36,25 @@ class Teacher implements TeacherInterface {
   }
 }
 
-function createEmployee (salary: number | string): Director | Teacher {
-  if (typeof(salary) === 'number' && salary < 500)  {
-    return new Teacher;
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500)  {
+    return new Teacher();
   } else {
-    return new Director;
+    return new Director();
   }
 }
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+} 
+
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+executeWork(createEmployee(200));   // Outputs: Getting to work
+executeWork(createEmployee(1000));  // Outputs: Getting to director tasks
