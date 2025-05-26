3f5a814cb880d36b00c0c71a82304de86ec94336
korneel <korneel.loy@gmail.com>
2025-05-26 12:09:45 +0200

    typescript ex 2

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,8 +1,12 @@
-interface Teacher{
+interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
   [any_attribute: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
 }
